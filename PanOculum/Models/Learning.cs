4f1d23eea79ef9f6b9b53b6ec865c446be2eac8f
YusifE <yusifelawawdeh@gmail.com>
2017-09-30 17:43:43 -0700

    added learning model

---
+++ PanOculum/Models/Learning.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PanOculum.Models
+{
+    public class Learning
+    {
+        public int Id { get; set; }
+        public string CurriculumTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public string[] Courses { get; set; }
+        public bool Favorate { get; set; }
+    }
+}
